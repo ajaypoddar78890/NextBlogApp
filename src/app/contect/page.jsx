ac8816d9aa428966bcc61f064182eb8c87008b0e
Your GitHub Username <ajay.poddar15fea@gmai.com>
2024-01-09 03:30:12 +0530

    after fatal the push problem

---
+++ src/app/contect/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contect = () => {
+  return (
+    <div>
+         <h2>contect</h2>
+    </div>
+  )
+}
+
+export default  contect
---
+++ src/component/header/page.jsx
@@ -0,0 +1,28 @@
+ import React from 'react'
+ 
+ const page = () => {
+   return (
+     <div className='flex justify-between h-10 w-full '>
+          <div className='  '>logo</div>
+
+           <div className=''>
+           <ol className='flex gap-20 text-red-500'>
+            <li>Blog</li>
+            <li>About</li>
+            <li>Contect</li>
+            <li>Details</li>
+             
+           </ol>
+           </div>
+
+           <div className="icon">
+             <ol>
+              <li>icon</li>
+             </ol>
+           </div>
+     </div>
+   )
+ }
+ 
+ export default page
+ 
